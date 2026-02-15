ecd29e66902d40c322fb4a9e542db43873871956
brightreuben2007 <brightreuben2007@gmail.com>
2026-02-15 20:25:07 +0100

    JavaAssignment1

---
+++ JavaAssignment1.java
@@ -0,0 +1,120 @@
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+public static void main(String[] args){
+
+
+shortnote: 
+ public
+- An access modifier. It means the method can be accessed from anywhere in the program.
+
+static
+- Means the method belongs to the class itself, not an object of the class.
+- Java can run this method without creating an instance of the class.
+
+void
+- The return type. It means this method does not return any value.
+
+main
+- The starting point of every Java program.
+- When you run a Java program, execution begins from this method.
+
+String[] args
+- An array of strings used to store command-line arguments passed when running the program.
